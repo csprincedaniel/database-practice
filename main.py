b9eece73d8d5ad8c589df5962484693ed92ff0a1
csprincedaniel <csprincedaniel@gmail.com>
2026-04-24 20:58:24 -0500

    V1

---
+++ main.py
@@ -0,0 +1,32 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+
+def create_table():
+    conn = psycopg2.connect(dbname=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE students(id serial primary key, name text, addrss text, age int, number text);")
+    print("Student table created")
+    conn.commit()
+
+    conn.close()
+
+
+def insert_data():
+    #accept data from user
+    name = input("Enter name: ")
+    address = input("Enter address: ")
+    age = input("Enter age: ")
+    number = input("Enter number: ")
+    conn = psycopg2.connect(dbname=os.getenv("DB_NAME"), user=os.getenv("DB_USER"), password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
+    cur = conn.cursor()
+    cur.execute("INSERT into students(name, addrss, age, number) values(%s, %s, %s, %s)",(name, address, age, number))
+    print("Data added to student table")
+    conn.commit()
+    conn.close()
+    
+insert_data()
