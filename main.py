46777aa1223b9051dec40f263a77d3f413859a00
csprincedaniel <csprincedaniel@gmail.com>
2026-04-25 16:49:30 -0500

    interface update

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from tkinter import *
 
 root = Tk()
 root.title("Student Database Management System")
-
+#root.geometry("300x300")
 frame = LabelFrame(root, text="Student Data")
+frame.config(bg="grey")
 
 frame.grid(row=0, column=0, padx=10, pady=10, sticky="ew")
 
@@ -23,5 +24,13 @@ age.grid(row=2, column=1, pady=2, sticky="ew")
 Label(frame, text="Number:").grid(row=3, column=0, sticky="w")
 number = Entry(frame)
 number.grid(row=3, column=1, pady=2, sticky="ew")
+
+button_frame = Frame(root)
+button_frame.grid(row=1, column=0, pady=5, sticky="ew")
+
+Button(button_frame, text="Create Table").grid(row=0, column=0)
+Button(button_frame, text="Add Data").grid(row=0, column=1)
+Button(button_frame, text="Update Data Table").grid(row=0, column=2)
+Button(button_frame, text="Delete Data").grid(row=0, column=3)
 
 root.mainloop()
